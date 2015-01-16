94944fbbf5b11a2e2ea66d94c21e1ef3d5a5ba8b
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-01-17 00:01:19 +0100

    Split up todolist.py into modules.

---
+++ app/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from config import config
+
+
+db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    from .api import api as api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api')
+
+    return app
---
+++ app/models.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask.ext.login import UserMixin
+
+from . import db
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(64), unique=True)
+    password_hash = db.Column(db.String(128))
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    todos = db.relationship('Todo', backref='creator', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(140))
+    is_completed = db.Column(db.Boolean, nullable=False, default=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, description, creator_id):
+        self.description = description
+        self.creator_id = creator_id
+
+    def __repr__(self):
+        creator = User.query.filter_by(id=self.creator_id)
+        return '<Todo: %r from %r>' % (self.description, creator.username)
