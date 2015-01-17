4d849c6aa91626ea7e6327a51a49fde690210ac4
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-01-17 14:44:21 +0100

    Changed config.py to provide different settings.

--- config.py
+++ config.py
@@ -1,3 +1,43 @@
-DEBUG=True
-SQLALCHEMY_DATABASE_URI='sqlite:///todolist.db'
-SECRET_KEY='development-key'
+# -*- coding: utf-8 -*-
+
+import os
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY ='development-key'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_RECORD_QUERIES = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR,
+                                                          'todolist-dev.db')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR,
+                                                          'todolist-test.db')
+    # WTF_CSRF_ENABLED = False
+    import logging
+    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+    logging.getLogger().setLevel(logging.DEBUG)
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR,
+                                                          'todolist.db')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
