ac5c4e98ad4d0187f935ef9b2cfab5b15fb80032
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-01-24 17:22:49 +0100

    Renamed new_todolist to add_todolist.

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template, redirect, request, url_for, flash
+from flask import render_template, redirect, request, url_for
+from flask.ext.login import current_user
 
 from . import main
 from .forms import TodoForm
@@ -11,7 +12,7 @@ from ..models import User, Todo, TodoList
 def index():
     form = TodoForm()
     if form.validate_on_submit():
-        return redirect(url_for('main.new_todolist'))
+        return redirect(url_for('main.add_todolist'))
     return render_template('index.html', form=form)
 
 
@@ -22,6 +23,11 @@ def user(username):
     return render_template('user.html', user=user, todos=todos)
 
 
+@main.route('/user/<int:id>/todolists')
+def todolist_overview():
+    return render_template('overview.html')
+
+
 @main.route('/todolist/<int:id>', methods=['GET', 'POST'])
 def todolist(id):
     todolist = TodoList.query.filter_by(id=id).first_or_404()
@@ -29,15 +35,14 @@ def todolist(id):
     if form.validate_on_submit():
         Todo(form.todo.data, todolist.id).save()
         return redirect(url_for('main.todolist', id=id))
-        flash('Todo added.')
     return render_template('todolist.html', todolist=todolist, form=form)
 
 
-@main.route('/todolist/new', methods=['POST'])
-def new_todolist():
+@main.route('/todolist/add', methods=['POST'])
+def add_todolist():
     form = TodoForm(todo=request.form.get('todo'))
     if form.validate():
-        todolist = TodoList("").save()
+        todolist = TodoList("", current_user.get_id()).save()
         Todo(form.todo.data, todolist.id).save()
         return redirect(url_for('main.todolist', id=todolist.id))
     return redirect(url_for('main.index'))
