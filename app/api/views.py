8247dc86a13e3c079d96e734c1a4bb1a480eacc2
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-01-24 17:21:44 +0100

    Start user json with 'user':

--- app/api/views.py
+++ app/api/views.py
@@ -10,14 +10,14 @@ from ..models import User, TodoList
 def get_users():
     users = User.query.all()
     return jsonify({
-        'users' : [user.to_json() for user in users]
+        'users' : [{'user': user.to_json()} for user in users]
     })
 
 
 @api.route('/user/<int:id>')
 def get_user(id):
     user = User.query.get_or_404(id)
-    return jsonify(user.to_json())
+    return jsonify({'user' : user.to_json()})
 
 
 @api.route('/user/<int:id>/todolists')
@@ -32,7 +32,6 @@ def get_user_todolists(id):
 @api.route('/user/<int:user_id>/todolist/<int:todolist_id>')
 def get_todolist_todos(todolist_id, user_id=None):
     todolist = TodoList.query.get_or_404(todolist_id)
-    todos = todolist.todos
     return jsonify({
-        'todos': [todo.to_json() for todo in todos]
+        'todos': [todo.to_json() for todo in todolist.todos]
     })
