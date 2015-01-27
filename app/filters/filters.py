8ea6c4560c18f11c61f353df331d0a7b231c07a7
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-01-27 22:44:49 +0100

    Switched from humanize to Flask Snippet 33.

--- app/filters/filters.py
+++ app/filters/filters.py
@@ -1,10 +1,43 @@
 # -*- coding: utf-8 -*-
 
-import humanize
+from datetime import datetime
 
 from . import filters_blueprint
 
 
+# Thanks to Dan Jacob and Sean Vieira for making the following snippet
+# available under http://flask.pocoo.org/snippets/33/
 @filters_blueprint.app_template_filter('humanize')
-def humanize_time(datetime):
-    return humanize.naturaltime(datetime)
+def humanize_time(dt, past_="ago", future_="from now", default="just now"):
+    """
+    Returns string representing "time since"
+    or "time until" e.g.
+    3 days ago, 5 hours from now etc.
+    """
+
+    now = datetime.utcnow()
+    if now > dt:
+        diff = now - dt
+        dt_is_past = True
+    else:
+        diff = dt - now
+        dt_is_past = False
+
+    periods = (
+        (diff.days / 365, "year", "years"),
+        (diff.days / 30, "month", "months"),
+        (diff.days / 7, "week", "weeks"),
+        (diff.days, "day", "days"),
+        (diff.seconds / 3600, "hour", "hours"),
+        (diff.seconds / 60, "minute", "minutes"),
+        (diff.seconds, "second", "seconds"),
+    )
+
+    for period, singular, plural in periods:
+
+        if period:
+            return "%d %s %s" % (period, \
+                singular if period == 1 else plural, \
+                past_ if dt_is_past else future_)
+
+    return default
