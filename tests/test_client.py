76776bafca138ecd75f0a52e2bdf8aaa808ec5a2
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-02-02 22:02:52 +0100

    Added more client test cases.

--- tests/test_client.py
+++ tests/test_client.py
@@ -22,34 +22,64 @@ class TodolistClientTestCase(unittest.TestCase):
         db.drop_all()
         self.app_context.pop()
 
+    def register_user(self, name):
+        response = self.client.post(url_for('auth.register'), data={
+            'username': name,
+            'email': name +'@example.com',
+            'password': 'correcthorsebatterystaple',
+            'password_confirmation': 'correcthorsebatterystaple'
+        })
+        return response
+
+    def login_user(self, name):
+        response = self.client.post(url_for('auth.login'), data={
+            'email': name + '@example.com',
+            'password': 'correcthorsebatterystaple'
+        })
+        return response
+
+    def register_and_login(self, name):
+        response = self.register_user(name)
+        self.assertEqual(response.status_code, 302)
+        response = self.login_user(name)
+        self.assertEqual(response.status_code, 302)
+
     def test_home_page(self):
         response = self.client.get(url_for('main.index'))
         self.assertTrue(b'Todolists' in response.data)
 
-    def test_register(self):
+    def test_register_page(self):
+        response = self.client.get(url_for('auth.register'))
+        self.assertTrue(b'Already registerd?' in response.data)
+
+    def test_login_page(self):
+        response = self.client.get(url_for('auth.login'))
+        self.assertTrue(b'New user?' in response.data)
+
+    def test_overview_redirect_if_user_not_logged_in(self):
+        response = self.client.get(url_for('main.todolist_overview'))
+        # expect redirect to index, because user is not logged in
+        self.assertEqual(response.status_code, 302)
+
+    def test_overview_redirect_if_user_logged_in(self):
+        self.register_and_login('adam')
+        response = self.client.get(url_for('main.todolist_overview'))
+        # expect not redirect as user is logged in
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_and_login_and_logout(self):
         # register a new account
-        response = self.client.post(url_for('auth.register'), data={
-            'email': 'john@example.com',
-            'username': 'john',
-            'password': 'correcthorsebatterystaple',
-            'password_confirmation': 'correcthorsebatterystaple'
-        })
-        self.assertTrue(response.status_code == 200)
+        response = self.register_user('adam')
+        # expect redirect to login
+        self.assertEqual(response.status_code, 302)
 
-    def test_login(self):
         # login with the new account
-        response = self.client.post(url_for('auth.login'), data={
-            'email': 'john@example.com',
-            'password': 'correcthorsebatterystaple'
-        })
-        self.assertTrue(response.status_code == 200)
+        response = self.login_user('adam')
+        # expect redirect to index
+        self.assertEqual(response.status_code, 302)
 
-    def test_logout(self):
-        # login with the new account
-        response = self.client.post(url_for('auth.login'), data={
-            'email': 'john@example.com',
-            'password': 'correcthorsebatterystaple'
-        }, follow_redirects=True)
-        # log out
-        response = self.client.get(url_for('auth.logout'), follow_redirects=True)
-        self.assertTrue(response.status_code == 200)
+        # logout
+        response = self.client.get(url_for('auth.logout'),
+                                   follow_redirects=True)
+        # follow redirect to index
+        self.assertEqual(response.status_code, 200)
