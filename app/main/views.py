f1ab512e836f771e8466dd6de93fa8a3fac417a0
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-02-05 23:24:17 +0100

    Changed default title of untitled todolists.

--- app/main/views.py
+++ app/main/views.py
@@ -41,7 +41,8 @@ def todolist(id):
 def new_todolist():
     form = TodoForm(todo=request.form.get('todo'))
     if form.validate():
-        todolist = TodoList("", current_user.get_id()).save()
+        todolist = TodoList(title="untitled",
+                            creator_id=current_user.get_id()).save()
         Todo(form.todo.data, todolist.id).save()
         return redirect(url_for('main.todolist', id=todolist.id))
     return redirect(url_for('main.index'))
