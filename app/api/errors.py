2118fd198c79163a1c6cc436059f56984a72488e
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-02-05 23:22:59 +0100

    Extracted errors from API view.

---
+++ app/api/errors.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from flask import jsonify, make_response
+
+from . import api
+
+
+@api.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad Request'}), 400)
+
+
+@api.errorhandler(401)
+def unauthorized(error):
+    return make_response(jsonify({'error': 'Unauthorized'}), 401)
+
+
+@api.errorhandler(403)
+def forbidden(error):
+    return make_response(jsonify({'error': 'Forbidden'}), 403)
+
+
+@api.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+def internal_server_error(error):
+    return make_response(jsonify({'error': 'Internal Server Error'}), 500)
