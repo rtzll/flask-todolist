ff5e9efef3c8e2ad8dc112fba3339cd03dd4be0b
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-04-19 14:19:16 +0200

    Changed humanize_time for python3.

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -17,6 +17,8 @@ def humanize_time(dt, past_='ago', future_='from now', default='just now'):
     """
 
     now = datetime.utcnow()
+    # remove tzinfo
+    dt = dt.replace(tzinfo=None)
     if now > dt:
         diff = now - dt
         dt_is_past = True
@@ -25,21 +27,23 @@ def humanize_time(dt, past_='ago', future_='from now', default='just now'):
         dt_is_past = False
 
     periods = (
-        (diff.days / 365, 'year', 'years'),
-        (diff.days / 30, 'month', 'months'),
-        (diff.days / 7, 'week', 'weeks'),
+        (diff.days // 365, 'year', 'years'),
+        (diff.days // 30, 'month', 'months'),
+        (diff.days // 7, 'week', 'weeks'),
         (diff.days, 'day', 'days'),
-        (diff.seconds / 3600, 'hour', 'hours'),
-        (diff.seconds / 60, 'minute', 'minutes'),
+        (diff.seconds // 3600, 'hour', 'hours'),
+        (diff.seconds // 60, 'minute', 'minutes'),
         (diff.seconds, 'second', 'seconds'),
     )
 
     for period, singular, plural in periods:
 
         if period:
-            return '%d %s %s' % (period, \
-                singular if period == 1 else plural, \
-                past_ if dt_is_past else future_)
+            return '%d %s %s' % (
+                period,
+                singular if period == 1 else plural,
+                past_ if dt_is_past else future_
+            )
 
     return default
 
