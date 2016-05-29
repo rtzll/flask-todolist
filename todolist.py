f2b19536ef6bd1000eb44bd837ecba74c9146413
Christian Rotzoll <ch.rotzoll@gmail.com>
2016-05-29 18:32:35 +0200

    Clean up todolist.py

--- todolist.py
+++ todolist.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from flask_sqlalchemy import SQLAlchemy
-
 from app import create_app, db
 
 app = create_app('development')
@@ -18,12 +16,8 @@ def test():
 @app.cli.command()
 def fill_db():
     """Fill database with random data.
-    By default 100 users, 400 todolists and 1600 todos.
+    By default 10 users, 40 todolists and 160 todos.
     WARNING: will delete existing data. For testing purposes only.
     """
     from utils.fake_generator import FakeGenerator
     FakeGenerator().start()  # side effect: deletes existing data
-
-
-if __name__ == '__main__':
-    manager.run()
