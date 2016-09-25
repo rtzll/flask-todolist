0f1dedda6a82ae1f2ab8fd679b1f5607668ce7bb
Christian Rotzoll <ch.rotzoll@gmail.com>
2016-09-25 22:24:14 +0200

    Add fallback SECRET_KEY for testing

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ['SECRET_KEY']
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret key, just for testing'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_RECORD_QUERIES = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
