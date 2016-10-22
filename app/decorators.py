143ad06a39a4ee971f3f65e58cf17fbf8ebc81cd
Christian Rotzoll <ch.rotzoll@gmail.com>
2016-10-22 19:38:58 +0200

    Abort when user is not authenticated or not an admin

--- app/decorators.py
+++ app/decorators.py
@@ -8,7 +8,7 @@ def admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         from flask_login import current_user
-        if not current_user.is_authenticated and not current_user.is_admin:
+        if not current_user.is_authenticated or not current_user.is_admin:
             abort(403)
         return f(*args, **kwargs)
     return decorated_function
