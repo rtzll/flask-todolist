91e9b7faf9d287dea71f61dd709421a0402f5a2d
Christian Rotzoll <ch.rotzoll@gmail.com>
2016-10-23 19:23:12 +0200

    Extract username string into variable
    Also simplify test logic, so only one DB query is used

--- tests/test_client.py
+++ tests/test_client.py
@@ -15,6 +15,7 @@ class TodolistClientTestCase(TestCase):
 
     def setUp(self):
         db.create_all()
+        self.username_alice = 'alice'
 
     def tearDown(self):
         db.session.remove()
@@ -58,27 +59,28 @@ class TodolistClientTestCase(TestCase):
         self.assert_template_used('login.html')
 
     def test_overview_page(self):
-        self.register_and_login('adam')
+        self.register_and_login(self.username_alice)
         response = self.client.get(url_for('main.todolist_overview'))
         # expect not redirect as user is logged in
         self.assert_200(response)
         self.assert_template_used('overview.html')
 
     def test_last_seen_update_after_login(self):
-        self.register_user('adam')
-        before = User.query.filter_by(username='adam').first().last_seen
-        self.login_user('adam')
-        after = User.query.filter_by(username='adam').first().last_seen
+        self.register_user(self.username_alice)
+        user = User.query.filter_by(username=self.username_alice).first()
+        before = user.last_seen
+        self.login_user(self.username_alice)
+        after = user.last_seen
         self.assertNotEqual(before, after)
 
     def test_register_and_login_and_logout(self):
         # register a new account
-        response = self.register_user('adam')
+        response = self.register_user(self.username_alice)
         # expect redirect to login
         self.assert_redirects(response, '/auth/login')
 
         # login with the new account
-        response = self.login_user('adam')
+        response = self.login_user(self.username_alice)
         # expect redirect to index
         self.assert_redirects(response, '/')
 
