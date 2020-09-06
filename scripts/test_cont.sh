4e2ca6ef3577d9a77cd6406b268e34af0c0f40b2
Usama42 <36984210+Usama42@users.noreply.github.com>
2020-09-06 14:37:43 +0500

    Update and rename test_cont.ps1 to test_cont.sh

--- scripts/test_cont.sh
+++ scripts/test_cont.sh
@@ -1,7 +1,7 @@
 $count = 0
 do {
     $count++
-    Write-Output  "[$env:STAGE_NAME] Staring container [Attempt: $count]"
+    echo "[$env:STAGE_NAME] Staring container [Attempt: $count]"
     $testStart = Invoke-WebRequest -Uri http://localhost:8000
 
     if ($testStart.statuscode -eq '200') {
